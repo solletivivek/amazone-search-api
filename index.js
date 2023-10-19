1d6ae8c6e19623a7b4ad3dab7777c581752409e8
vivek <110154461+solletivivek@users.noreply.github.com>
2023-10-19 18:36:51 +0530

    s

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-//const apiKey = '2c9b9d5304f0e97477158939e4f6fa90';
-//const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
-
-const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
-
 app.use(express.json());
 
+const apiKey = '2c9b9d5304f0e97477158939e4f6fa90';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.get('/', (req, res) => {
     res.send('hi World!');
@@ -19,9 +15,8 @@ app.get('/', (req, res) => {
 // GET Product Details
 app.get('/products/:productId', async (req, res) => {
     const { productId } = req.params;
-    const { api_key } = req.query;
     try {
-        const html = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+        const html = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
         const result = JSON.parse(html);
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result, null, 2)); // 2 spaces for indentation
@@ -33,9 +28,8 @@ app.get('/products/:productId', async (req, res) => {
 // GET Product review
 app.get('/products/:productId/reviews', async (req, res) => {
     const { productId } = req.params;
-    const { api_key } = req.query;
     try {
-        const html = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`);
+        const html = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
         const result = JSON.parse(html);
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result, null, 2)); // 2 spaces for indentation
@@ -47,9 +41,8 @@ app.get('/products/:productId/reviews', async (req, res) => {
 // GET Product offers
 app.get('/products/:productId/offers', async (req, res) => {
     const { productId } = req.params;
-    const { api_key } = req.query;
     try {
-        const html = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        const html = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
         const result = JSON.parse(html);
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result, null, 2)); // 2 spaces for indentation
@@ -61,9 +54,8 @@ app.get('/products/:productId/offers', async (req, res) => {
 // GET search results
 app.get('/search/:searchQuery', async (req, res) => {
     const { searchQuery } = req.params;
-    const { api_key } = req.query;
     try {
-        const html = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        const html = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
         const result = JSON.parse(html);
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result, null, 2)); // 2 spaces for indentation
